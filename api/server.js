7ee49d82d5ba1d88d425a3532900bc90f64ec451
rcbjBlueMars <9321971+rcbjBlueMars@users.noreply.github.com>
2025-10-23 07:55:44 -0700

    Code clean up. Eliminating duplicate code between server & client side.

--- api/server.js
+++ api/server.js
@@ -5,18 +5,20 @@
 //
 'use strict';
 
+var appconfig = require(process.env.CONFIG_FILE);
 const express = require('express');
 const expressLogging = require('express-logging');
 const bunyan = require("bunyan");
 const axios = require('axios');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const { convertToOAuth2Format  } = require('./data.js');
 
 // Constants
-const PORT = process.env.PORT || 4000;
-const HOST = process.env.HOST || '0.0.0.0';
-const LOG_LEVEL = process.env.LOG_LEVEL || 'debug';
-const uiUrl = 'http://localhost:3000';
+const PORT = appconfig.port || 4000;
+const HOST = appconfig.host || '0.0.0.0';
+const LOG_LEVEL = appconfig.logLevel || 'debug';
+const uiUrl = appconfig.uiUrl || 'http://localhost:3000';
 
 var log = bunyan.createLogger({ name: 'server',
                                 level: LOG_LEVEL });
@@ -114,124 +116,26 @@ app.post('/token', (req, res) => {
     log.info('Entering app.post for /token.');
     const body = req.body;
     log.debug('body: ' + JSON.stringify(body));
-    var grantType = body.grant_type;  //=authorization_code
-    var clientId = body.client_id;  //=5qqbus6ukft6srjgqlijvk2465
-    var code = body.code; //=2a795117-43d5-4d4c-bdd6-0fc9632c0594
-    var redirectUri = body.redirect_uri; //=http%3A%2F%2Flocalhost%3A3000%2Fcallback
-    var scope = body.scope || ""; //=openid+email+phone+profile
-    var tokenEndpoint = body.token_endpoint; //=https%3A%2F%2Fblogpost1.auth.us-west-2.amazoncognito.com%2Foauth2%2Ftoken
-    var sslValidate = body.sslValidate; //=true
-    var clientSecret = encodeURIComponent(body.client_secret); //=tester
-    var username = body.username || "";
-    var password = body.password || "";
-    var refreshToken = body.refresh_token || "";
-    var resource = body.resource || "";
-    var customParams = body.customParams || {}; 
-    var code_verifier = body.code_verifier;
-    var auth_style = body.auth_style;
- 
-    log.debug('grantType: ' + grantType);
-    log.debug('clientId: ' + clientId);
-    log.debug('code: ' + code);
-    log.debug('redirectUri: ' + redirectUri);
-    log.debug('scope: ' + scope);
-    log.debug('tokenEndpoint: ' + tokenEndpoint);
-    log.debug('sslValidate: ' + sslValidate);
-    log.debug('clientSecret: ' + clientSecret);
-    log.debug('username: ' + username);
-    log.debug('password: ' + password);
-    log.debug('refreshToken: ' + refreshToken);
-    log.debug('resource: ' + resource);
-    Object.keys(customParams).forEach( (key) => {
-      log.debug(key + ':' + customParams[key]);
-    });
-    log.debug("code_verifier: " + code_verifier);
-    log.debug("auth_style: " + auth_style);
-    var parameterObject = {};
-    if(grantType == "authorization_code") {
-      parameterObject = { 
-  	grant_type: grantType,
-        client_id: clientId,
-  	code: code,
-  	redirect_uri: redirectUri,
-      };
-      if (typeof code_verifier != "undefined") {
-        parameterObject.code_verifier = code_verifier
-      }
-      log.debug("clientSecret: " + clientSecret);
-      log.debug("auth_style: " + auth_style);
-      if (!!clientSecret && auth_style) {
-        parameterObject.client_secret = clientSecret;
-      }
-    } else if(grantType == "client_credentials") {
-       parameterObject =  {
-         grant_type: grantType
-       };
-       log.debug("clientSecret: " + clientSecret);
-       log.debug("auth_style: " + auth_style);
-       if ((typeof clientSecret != "undefined" && clientSecret != "undefined")
-          && auth_style) {
-         parameterObject.client_secret = clientSecret;
-       }
-    } else if(grantType == "password") {
-       parameterObject = {
-  	grant_type: grantType,
-  	username: username,
-  	password: password
-       };
-    } else if(grantType == "refresh_token") {
-      parameterObject = {
-        grant_type: grantType,
-        client_id: clientId,
-        refresh_token: refreshToken,
-      };
-      log.debug("clientSecret: " + clientSecret);
-      log.debug("auth_style: " + auth_style);
-      if ((typeof clientSecret != "undefined" && clientSecret != "undefined")
-         && auth_style) {
-        parameterObject.client_secret = clientSecret;
-      }
-    }
-    if(auth_style) {
-        parameterObject.client_id = clientId;
-    }
-    if(resource != "") {
-      parameterObject.resource = resource;
-    }
-
-    if(scope != "") {
-      parameterObject.scope = scope;
-    }
-
-    if (Object.keys(customParams).length > 0) {
-      Object.keys(customParams).forEach( (key) => {
-        parameterObject[key] = customParams[key];
-      });
-    }
-    log.debug("parameterObject: " + JSON.stringify(parameterObject));
-  
-    var parameterString = "";
-    Object.keys(parameterObject).forEach( (key) => {
-      parameterString = parameterString +
-                      key +
-                      "=" +
-                      parameterObject[key] +
-                      "&";
-    });
-
+    const parameterString = convertToOAuth2Format(body);
     var headers = {
       'content-type' : 'application/x-www-form-urlencoded'
     };
+    const grantType = body.grant_type;
+    const clientSecret = encodeURIComponent(body.client_secret);
+    const code_verifier = body.code_verifier;
     if ( typeof code_verifier != "undefined" ||
          (grantType == "refresh_token" &&
           !clientSecret)) {
       headers.origin = uiUrl;
-    } 
+    }
+    const auth_style = body.auth_style;
+    var clientId = body.client_id;
     if (!auth_style) {
       // Put client_id + client_secret in Authorization header
       headers.authorization = 'Basic ' + Buffer.from(clientId + ":" + clientSecret).toString('base64');
     }
-    parameterString = parameterString.substring(0, parameterString.length - 1);
+    var tokenEndpoint = body.token_endpoint;
+    var sslValidate = body.sslValidate; 
     log.debug("Making call to Token Endpoint.");
     log.debug("POST " + tokenEndpoint);
     log.debug("Headers: " + JSON.stringify(headers));
