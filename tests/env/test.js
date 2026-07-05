364096377652230911e36001380079cbc4c8c294
Liya Broeckelmann <liya@iyasec.io>
2026-07-05 16:09:27 -0700

    Change waitTime to 2000.

--- tests/env/test.js
+++ tests/env/test.js
@@ -1,6 +1,6 @@
 var config = {
   // Milliseconds Selenium waits for elements/conditions in the test scripts.
-  waitTime: 10000,
+  waitTime: 2000,
   // Bunyan log level for the test scripts (trace|debug|info|warn|error|fatal).
   LOG_LEVEL: 'info'
 };
